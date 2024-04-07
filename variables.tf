5c3cbb5ac58a6dcf670f8adb31d18b71c917b011
bseliga <148666749+bseliga@users.noreply.github.com>
2024-04-07 09:47:05 +0800

    fix varibale file

--- variables.tf
+++ variables.tf
@@ -10,8 +10,10 @@ variable "ami_filter" {
     name  = string
     owner = string
   })
-  name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-  owner = "979382823631" # Bitnami
+  default = {
+    name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
 }
 
 variable "environment" {
@@ -21,17 +23,10 @@ variable "environment" {
     name = string
     network_prefix = string
   })
+
   default = {
     name = "dev"
     cidr = "10.0"
-  }
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
   }
 }
 
